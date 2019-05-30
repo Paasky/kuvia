7dd8f1ff6b5545cbb510d0ba58224fa1fe89bffe
Paasky <pekko.tuomisto@gmail.com>
2019-05-30 18:30:56 +0100

    Fix guest missing &$tiles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -66,7 +66,8 @@
                         'url' => '/logout',
                     ],
                 ]; ?>
-            @elseauth
+            @endauth
+            @guest
                 <?php $tiles = [
 
                 ]; ?>
